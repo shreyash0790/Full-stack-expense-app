fbffc0994e9b0f984d2c9f17f88e754e9535077b
Shreyaskar <shreyash0790@gmail.com>
2023-08-08 22:58:29 +0530

    login apis

--- controllers/login.js
+++ controllers/login.js
@@ -3,13 +3,17 @@ const Users = require('../models/sign');
 exports.GetUser = async (req, res, next) => {
     try {
         const Email = req.query.Email;
+        const Password=req.query.Password;
 
-        const existingUser = await Users.findOne({ where: { Email: Email } });
-
+        const existingUser = await Users.findOne({ where: { Email: Email} });
         if (existingUser) {
-            return res.status(200).json({ User: 'login successful' });
+            if (existingUser.Password === Password) {
+                return res.status(200).json({ User: 'login successful' });
+            } else {
+                return res.status(401).json({ error: 'password incorrect' });
+            }
         } else {
-            return res.status(400).json({ User: null }); 
+            return res.status(404).json({ User: null }); 
         }
     } catch (err) {
         res.status(500).json({ error: 'Internal Server Error' });
--- public/js/login.js
+++ public/js/login.js
@@ -5,43 +5,59 @@ const userEx = document.querySelector('#userEx');
 
 myForm.addEventListener('submit', onSubmit);
 
-function onSubmit(e) {
+async function onSubmit(e) {
     e.preventDefault();
     const Email = EmailInput.value;
     const Password = PasswordInput.value;
-    if ( Email === '' || Password=== '') {
-        alert("input all fields");
-    }
-    else {
-        const formData = { Email: Email, Password: Password, };
-        getFormData(formData); 
+    
+    if (Email === '' || Password === '') {
+        alert("Input all fields");
+    } else {
+        const formData = { Email: Email, Password: Password };
+        await getFormData(formData);
         clearFields();
     }
-  }
+}
 
-  async function getFormData(formData) {
+async function getFormData(formData) {
     try {
-        console.log(formData);
         const response = await axios.get('http://localhost:5000/GetUser', { params: formData });
         const responseData = response.data;
 
-            alert("Login successful");
-       
-       
+        if (response.status === 200) {
+            clearErrorMessages();
+            displayErrorMessage('Login success !');
+            
+        }
     } catch (err) {
-        if (err.response && err.response.status === 400) {
-            const h3 = document.createElement('h3');
-            h3.className = 'h3';
-            h3.textContent = 'User Not Found !';
-            userEx.appendChild(h3); 
-            clearFields();
-        }else{
-            userEx.innerText = 'An error occurred';
+        clearErrorMessages();
+        if (err.response) {
+            if (err.response.status === 404) {
+                displayErrorMessage('User does not exist!');
+            } else if (err.response.status === 401) {
+                displayErrorMessage('Password incorrect!');
+            } else {
+                alert('An error occurred');
+            }
+        } else {
+            alert('An error occurred');
         }
     }
 }
+
 function clearFields() {
-    EmailInput.value='';
-     PasswordInput.value='';
-    
+    EmailInput.value = '';
+    PasswordInput.value = '';
+}
+function clearErrorMessages() {
+    while (userEx.firstChild) {
+        userEx.removeChild(userEx.firstChild);
+    }
+}
+
+function displayErrorMessage(message) {
+    const h3 = document.createElement('h3');
+    h3.className = 'h3';
+    h3.textContent = message;
+    userEx.appendChild(h3);
 }
