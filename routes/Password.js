31de5644646565d2744fcb498d06fca37e77a8c3
Shreyaskar <shreyash0790@gmail.com>
2023-08-14 21:58:17 +0530

    test

--- routes/Password.js
+++ routes/Password.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const Passwordcontroller=require();
+const Passwordcontroller=require('../controllers/PasswordReset');
 
 
-router.get('/password/forgotpassword', Passwordcontroller.PassReset);
+router.post('/password/forgotpassword', Passwordcontroller.PassReset);
 
 
 module.exports=router;
